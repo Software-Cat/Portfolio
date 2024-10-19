11051fedaf32c9ad2e56f4aaffbab3b83b7f849e
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2024-10-19 14:43:19 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
                 '9/16': '56.25%'
             },
             fontFamily: {
-                fontPrimary: ['Inter SemiBold', 'monospace'],
+                fontPrimary: ['Inter', 'monospace'],
                 fontSecondary: ['Azeret Mono', 'monospace']
             },
             letterSpacing: {
